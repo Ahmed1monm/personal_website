92ba6446d2b095234fca7cfaf72720d4b977cd08
Ahmed1monm <ahmedmoniem922@gmail.com>
2022-07-15 00:46:55 +0200

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
